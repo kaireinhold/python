91e3c7ce3b62ea255d04eb24cd6c5302d6130d6e
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-21 10:00:25 -0700

    made functions to set user level and increase stats. made 'mods' dictionary static instead of dynamic with each run of stat_roll function as it was causing issues

--- DnD_Game.py
+++ DnD_Game.py
@@ -8,7 +8,28 @@ for_stats = 0
 roll_stats = 5
 rolls = []
 stats = []
-mods = {}
+mods = {
+    1: -5,
+    2: -4,
+    3: -4,
+    4: -3,
+    5: -3,
+    6: -2,
+    7: -2,
+    8: -1,
+    9: -1,
+    10: 0,
+    11: 0,
+    12: 1,
+    13: 1,
+    14: 2,
+    15: 2,
+    16: 3,
+    17: 3,
+    18: 4,
+    19: 4,
+    20: 5
+    }
 stat_types = {}
 def roll(di=None):
     if di == None:
@@ -65,7 +86,6 @@ def stat_roll(u_class=None):
             elif len(rolls) == 4:
                 rolls.remove(min(rolls))
                 stats.append(sum(rolls))
-                mods[stats[-1]] = (stats[-1] - 10)//2
                 rolls = []
                 for_stats += 1
                 if for_stats >= 6:
@@ -214,9 +234,6 @@ def stat_roll(u_class=None):
         elif u_class.lower() == "andrew" or u_class.lower() == "luca" or u_class.lower() == "kai":
             #easter egg for friends/fellow dms :)
             stats = [20]
-            mods = {
-                20: 5
-                }
             stat_types = {
             "Str": stats[0],
             "Dex": stats[0],
@@ -244,9 +261,109 @@ def stat_roll(u_class=None):
         "Wis": stats[4],
         "Cha": stats[5]
         }
-
     return stat_types
-     
+
+def stat_increase():
+    inc_amount = int(input(""" How do you want to increase your stats?
+(1) Increase 1 stat by 2 points
+(2) Increase 2 stats by 1 point each
+"""))
+    if inc_amount == 1:
+        stat_to_inc = input("What stat do you want to increase? (Choose 1: Str, Dex, Con, Int, Wis, Cha) ")
+        counter = [1]
+        for x in counter:
+            if stat_to_inc.lower().strip() == "str":
+                if stat_types["Str"] == 20:
+                    continue
+                else:
+                    stat_types["Str"] += 2
+            elif stat_to_inc.lower().strip() == "dex":
+                if stat_types["Dex"] == 20:
+                    continue
+                else:
+                    stat_types["Dex"] += 2
+            elif stat_to_inc.lower().strip() == "con":
+                if stat_types["Con"] == 20:
+                    continue
+                else:
+                    stat_types["Con"] += 2
+            elif stat_to_inc.lower().strip() == "int":
+                if stat_types["Int"] == 20:
+                    continue
+                else:
+                    stat_types["Int"] += 2
+            elif stat_to_inc.lower().strip() == "wis":
+                if stat_types["Wis"] == 20:
+                    continue
+                else:
+                    stat_types["Wis"] += 2
+            elif stat_to_inc.lower().strip() == "cha":
+                if stat_types["Cha"] == 20:
+                    continue
+                else:
+                    stat_types["Cha"] += 2
+    elif inc_amount == 2:
+        stats_to_inc = input("What stats do you want to increase? (Choose 2, separate by a comma: Str, Dex, Con, Int, Wis, Cha) ")
+        inc_list = stats_to_inc.split(",")
+        for stat in inc_list:
+            if stat.lower().strip() == "str":
+                if stat_types["Str"] == 20:
+                    continue
+                else:
+                    stat_types["Str"] += 2
+            elif stat.lower().strip() == "dex":
+                if stat_types["Dex"] == 20:
+                    continue
+                else:
+                    stat_types["Dex"] += 2
+            elif stat.lower().strip() == "con":
+                if stat_types["Con"] == 20:
+                    continue
+                else:
+                    stat_types["Con"] += 2
+            elif stat.lower().strip() == "int":
+                if stat_types["Int"] == 20:
+                    continue
+                else:
+                    stat_types["Int"] += 2
+            elif stat.lower().strip() == "wis":
+                if stat_types["Wis"] == 20:
+                    continue
+                else:
+                    stat_types["Wis"] += 2
+            elif stat.lower().strip() == "cha":
+                if stat_types["Cha"] == 20:
+                    continue
+                else:
+                    stat_types["Cha"] += 2
+
+    print(stat_types)
+
+def set_level():
+    global user_level
+    user_level = int(input("What level is your character? (number) "))
+    if user_level >= 19:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif user_level >= 16:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif user_level >= 12:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif user_level >= 8:
+        stat_increase()
+        stat_increase()
+    elif user_level >= 4:
+        stat_increase()
+    return user_level
+
 while roll_stats > 4:
             rolls = []
             start = input("Start? (y/n) ").lower().strip()
@@ -254,19 +371,14 @@ while roll_stats > 4:
                 user_class = input("What class do you choose? (Barbarian, Fighter, Wizard, Rogue, Bard, Druid, Paladin, Cleric, Monk, Ranger, Sorcerer, Warlock, Artificer) ").strip()
                 roll_stats = 4
                 stat_roll(user_class)
+                print(stats)
+                print(stat_types)
+                set_level()
             else:
                 sys.exit()
-ref_stats = {
-        "Str": stats[0],
-        "Dex": stats[1],
-        "Con": stats[2],
-        "Int": stats[3],
-        "Wis": stats[4],
-        "Cha": stats[5]
-        }
 
-if user_class.lower() == "andrew" or user_class.lower() == "luca" or user_class.lower() == "kai":
-    print(f"""You are {user_class}!
+if user_class.lower() == "andrew" or user_class.lower() == "luca" or user_class.lower() == "kai" or user_class.lower() == "z" or user_class.lower() == "zurulien":
+    print(f"""You are {user_class}! Your level is {user_level}!
 Your stats are:
 Strength: {stat_types["Str"]} ({mods[stat_types["Str"]]})
 Dexterity: {stat_types["Dex"]} ({mods[stat_types["Dex"]]})
@@ -277,7 +389,7 @@ Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})
 """)
     
 elif user_class.lower() == "artificer":
-    print(f"""You are an {user_class}!
+    print(f"""You are an {user_class}! Your level is {user_level}!
 Your stats are:
 Strength: {stat_types["Str"]} ({mods[stat_types["Str"]]})
 Dexterity: {stat_types["Dex"]} ({mods[stat_types["Dex"]]})
@@ -287,8 +399,9 @@ Wisdom: {stat_types["Wis"]} ({mods[stat_types["Wis"]]})
 Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})
 """)
     
-elif stat_types == ref_stats:
-    print(f"""Your stats are:
+elif user_class == None or user_class == "" or user_class != 'barbarian' or user_class != 'fighter' or user_class != 'wizard' or user_class != 'rogue' or user_class != 'bard' or user_class != 'druid' or user_class != 'paladin' or user_class != 'cleric' or user_class != 'monk' or user_class != 'ranger' or user_class != 'sorcerer' or user_class != 'warlock' or user_class != 'artificer' or user_class != 'andrew' or user_class != 'luca' or user_class != 'kai' or user_class != 'z' or user_class != "zurulien":
+    print(f"""Your level is {user_level}!
+Your stats are:
 Strength: {stat_types["Str"]} ({mods[stat_types["Str"]]})
 Dexterity: {stat_types["Dex"]} ({mods[stat_types["Dex"]]})
 Constitution: {stat_types["Con"]} ({mods[stat_types["Con"]]})
@@ -298,7 +411,7 @@ Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})
 """)
     
 else:
-    print(f"""You are a {user_class}!
+    print(f"""You are a {user_class}! Your level is {user_level}!
 Your stats are:
 Strength: {stat_types["Str"]} ({mods[stat_types["Str"]]})
 Dexterity: {stat_types["Dex"]} ({mods[stat_types["Dex"]]})
