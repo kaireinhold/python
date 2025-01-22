9195ba29d4895ee7ccbb36182ba2bd1dfdf605fb
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-22 10:04:21 -0700

    added alignment

--- DnD_Game.py
+++ DnD_Game.py
@@ -468,6 +468,7 @@ while roll_stats > 4:
                 print(stat_types)
                 set_race()
                 set_level()
+                alignment = input("What is your alignment? (Chaotic Good, Neutral Good, Lawful Good, Lawful Neutral, True Neutral, Chaotic Neutral, Lawful Evil, Neutral Evil, Chaotic Evil) ")
             else:
                 sys.exit()
 
@@ -506,15 +507,16 @@ Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})
 """)
     
 else:
-    print(f"""You are a {user_class}! Your level is {user_level}!
+    print(f"""You are a {user_class}!
+Your level is {user_level}!
 Your stats are:
 Strength: {stat_types["Str"]} ({mods[stat_types["Str"]]})
 Dexterity: {stat_types["Dex"]} ({mods[stat_types["Dex"]]})
 Constitution: {stat_types["Con"]} ({mods[stat_types["Con"]]})
 Intelligence: {stat_types["Int"]} ({mods[stat_types["Int"]]})
 Wisdom: {stat_types["Wis"]} ({mods[stat_types["Wis"]]})
-Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})
-""")
+Charisma: {stat_types["Cha"]} ({mods[stat_types["Cha"]]})""")
+print(f"Your alignment is {alignment}!")
 
 if user_race.lower().strip() == "aasimar" or user_race.lower().strip() == "air genasi" or user_race.lower().strip() == "arachne" or user_race.lower().strip() == "aarakocra" or user_race.lower().strip() == "earth genasi" or user_race.lower().strip() == "elf":
     print(f"You are an {user_race}!")
