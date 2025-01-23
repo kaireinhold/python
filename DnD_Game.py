d6b9dc0f877496e39b125025d1755f1d5ca89e87
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-23 08:25:11 -0700

    changed mods dictionary to be one line instead of 22 lines because it was annoying lol

--- DnD_Game.py
+++ DnD_Game.py
@@ -8,28 +8,7 @@ for_stats = 0
 roll_stats = 5
 rolls = []
 stats = []
-mods = {
-    1: -5,
-    2: -4,
-    3: -4,
-    4: -3,
-    5: -3,
-    6: -2,
-    7: -2,
-    8: -1,
-    9: -1,
-    10: 0,
-    11: 0,
-    12: 1,
-    13: 1,
-    14: 2,
-    15: 2,
-    16: 3,
-    17: 3,
-    18: 4,
-    19: 4,
-    20: 5
-    }
+mods = {1: -5, 2: -4, 3: -4, 4: -3, 5: -3, 6: -2, 7: -2, 8: -1, 9: -1, 10: 0, 11: 0, 12: 1, 13: 1, 14: 2, 15: 2, 16: 3, 17: 3, 18: 4, 19: 4, 20: 5}
 stat_types = {}
 def roll(di=None):
     if di == None:
@@ -534,9 +513,12 @@ for x in languages:
 
 '''
 TBA:
-Core Bloodling
+Core Starling
 Shadeling
 Crystalling
+Bloodling
+Pure Starling
+Symbioling
 Silenced
 Starved
 '''
