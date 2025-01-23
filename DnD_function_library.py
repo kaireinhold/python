37b4ead4950c703736df01b82d1f99bbdb62aa3f
Kai Reinhold <keena.reinhold07@gmail.com>
2025-01-23 09:58:06 -0700

    Testing, need to save tests

---
+++ DnD_function_library.py
@@ -0,0 +1,500 @@
+#Created by Kai Reinhold (kaireinhold on GitHub)
+
+import random
+import time
+
+for_stats = 0
+roll_stats = 5
+rolls = []
+stats = []
+mods = {1: -5, 2: -4, 3: -4, 4: -3, 5: -3, 6: -2, 7: -2, 8: -1, 9: -1, 10: 0, 11: 0, 12: 1, 13: 1, 14: 2, 15: 2, 16: 3, 17: 3, 18: 4, 19: 4, 20: 5}
+stat_types = {}
+user_level = 0
+#user_class = ""
+user_race = ""
+movement_speed = 0
+darkvision = False
+languages = []
+
+def roll(di=None):
+    """
+    Simulate rolling dice with a specified number of sides and display the result.
+
+    Input:
+    - d<number> (str): A string where <number> represents the number of sides on the dice to roll, chosen by the user (e.g., 'd20' for a 20-sided die).
+
+    Output:
+    - str: A message indicating "Rolling d<number>..." followed by a wait of 1.5 seconds, and then another message displaying the rolled value (e.g., "d<number>: You rolled a <rolled value>!").
+
+    Behavior:
+    - Prompts the user to input a dice to roll (ie. 'd20' for a 20-sided dice.)
+    - Rolls the specified dice and prints the result.
+    - Waits for 1.5 seconds before displaying the result.
+    - Detects if the rolled number should be preceded by 'an' (for example, 8 or 18) and adjusts the output message accordingly.
+    - Appends the roll output for each roll to the list named 'rolls'
+    Raises:
+    - ValueError: If the input does not include the 'd' prefix, indicating the dice format.
+    
+    """
+    if di == None:
+        dice_chosen = input("What dice would you like to roll? (d[number] ")
+    else:
+        dice_chosen = di
+    dice_list = dice_chosen.lower().split()
+    an_list = [11, 18,]
+    for x in dice_list:
+        if "d" not in x:
+            raise ValueError("Value must have a 'd' in front to specify that it is a dice")
+    
+        print("Rolling "  + x + "...")
+        time.sleep(1.5)
+        roll_num = int(x[1:len(x)])
+        roll_output = random.randint(1,roll_num)
+        output_str = str(roll_output)
+        if roll_output in an_list or str(output_str[0]) == "8":
+            print(x + ": You rolled an", output_str + "!")
+        else:
+            print(x + ": You rolled a", output_str + "!")
+        time.sleep(1)
+        rolls.append(roll_output)
+
+def roll_no_output(di=None):
+    """
+    Simulate rolling dice with a specified number of sides and display the result.
+
+    Input:
+    - d<number> (str): A string where <number> represents the number of sides on the dice to roll, chosen by the user (e.g., 'd20' for a 20-sided die).
+
+    Output:
+    - None.
+
+    Behavior:
+    - Prompts the user to input a dice to roll (ie. 'd20' for a 20-sided dice.)
+    - Rolls the specified dice and prints the result.
+    - Waits for 1.5 seconds before displaying the result.
+    - Detects if the rolled number should be preceded by 'an' (for example, 8 or 18) and adjusts the output message accordingly.
+    - Appends the roll output for each roll to the list named 'rolls'
+    Raises:
+    - ValueError: If the input does not include the 'd' prefix, indicating the dice format.
+    
+    """
+    if di == None:
+        dice_chosen = input("What dice would you like to roll? (d[number] ")
+    else:
+        dice_chosen = di
+    dice_list = dice_chosen.lower().split()
+    an_list = [11, 18,]
+    for x in dice_list:
+        if "d" not in x:
+            raise ValueError("Value must have a 'd' in front to specify that it is a dice")
+    
+        roll_num = int(x[1:len(x)])
+        roll_output = random.randint(1,roll_num)
+        rolls.append(roll_output)
+        
+def stat_roll(u_class=None):
+    global for_stats
+    global roll_stats
+    global rolls
+    global stat_types
+    global stats
+    while for_stats < 6:
+        while roll_stats > 0:
+            roll_stats -= 1
+            roll_no_output("d6")
+            if rolls[-1] == 1:
+                rolls.pop()
+                roll_stats += 1
+            elif len(rolls) == 4:
+                rolls.remove(min(rolls))
+                stats.append(sum(rolls))
+                rolls = []
+                for_stats += 1
+                if for_stats >= 6:
+                    break
+                roll_stats = 5
+    if u_class == None or u_class == "":
+        stat_types = {
+        "Str": stats[0],
+        "Dex": stats[1],
+        "Con": stats[2],
+        "Int": stats[3],
+        "Wis": stats[4],
+        "Cha": stats[5]
+        }
+    else:
+        if u_class.lower() == "barbarian":
+            stats.sort()
+            stat_types = {
+            "Str": stats[5],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[0],
+            "Wis": stats[2],
+            "Cha": stats[1]
+            }
+        elif u_class.lower() == "fighter":
+            stats.sort()
+            stat_types = {
+            "Str": stats[5],
+            "Dex": stats[1],
+            "Con": stats[3],
+            "Int": stats[4],
+            "Wis": stats[0],
+            "Cha": stats[2]
+            }
+        elif u_class.lower() == "wizard":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[4],
+            "Con": stats[3],
+            "Int": stats[5],
+            "Wis": stats[2],
+            "Cha": stats[1]
+            }
+        elif u_class.lower() == "rogue":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[5],
+            "Con": stats[4],
+            "Int": stats[1],
+            "Wis": stats[2],
+            "Cha": stats[3]
+            }
+        elif u_class.lower() == "bard":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[4],
+            "Con": stats[3],
+            "Int": stats[1],
+            "Wis": stats[2],
+            "Cha": stats[5]
+            }
+        elif u_class.lower() == "druid":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[2],
+            "Wis": stats[5],
+            "Cha": stats[1]
+            }
+        elif u_class.lower() == "paladin":
+            stats.sort()
+            stat_types = {
+            "Str": stats[5],
+            "Dex": stats[1],
+            "Con": stats[3],
+            "Int": stats[0],
+            "Wis": stats[2],
+            "Cha": stats[4]
+            }
+        elif u_class.lower() == "cleric":
+            stats.sort()
+            stat_types = {
+            "Str": stats[3],
+            "Dex": stats[2],
+            "Con": stats[4],
+            "Int": stats[1],
+            "Wis": stats[5],
+            "Cha": stats[0]
+            }
+        elif u_class.lower() == "monk":
+            stats.sort()
+            stat_types = {
+            "Str": stats[4],
+            "Dex": stats[2],
+            "Con": stats[3],
+            "Int": stats[1],
+            "Wis": stats[5],
+            "Cha": stats[0]
+            }
+        elif u_class.lower() == "ranger":
+            stats.sort()
+            stat_types = {
+            "Str": stats[5],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[1],
+            "Wis": stats[2],
+            "Cha": stats[0]
+            }
+        elif u_class.lower() == "sorcerer":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[2],
+            "Wis": stats[1],
+            "Cha": stats[5]
+            }
+        elif u_class.lower() == "warlock":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[1],
+            "Wis": stats[2],
+            "Cha": stats[5]
+            }
+        elif u_class.lower() == "artificer":
+            stats.sort()
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[3],
+            "Con": stats[4],
+            "Int": stats[5],
+            "Wis": stats[2],
+            "Cha": stats[1]
+            }
+        elif u_class.lower() == "andrew" or u_class.lower() == "luca" or u_class.lower() == "kai":
+            #easter egg for friends/fellow dms :)
+            stats = [20]
+            stat_types = {
+            "Str": stats[0],
+            "Dex": stats[0],
+            "Con": stats[0],
+            "Int": stats[0],
+            "Wis": stats[0],
+            "Cha": stats[0]
+            }
+        elif u_class.lower() == "z" or u_class.lower() == "zurulien":
+            stats.sort()
+            stat_types = {
+            "Str": stats[4],
+            "Dex": stats[0],
+            "Con": stats[1],
+            "Int": stats[5],
+            "Wis": stats[3],
+            "Cha": stats[2]
+            }
+        else:
+            stat_types = {
+        "Str": stats[0],
+        "Dex": stats[1],
+        "Con": stats[2],
+        "Int": stats[3],
+        "Wis": stats[4],
+        "Cha": stats[5]
+        }
+    print(stat_types)
+    return stat_types
+
+def stat_increase(inc_amount = None):
+    if inc_amount == None:
+        inc_amount = int(input(""" How do you want to increase your stats?
+(1) Increase 1 stat by 2 points
+(2) Increase 2 stats by 1 point each
+"""))
+    if inc_amount == 1:
+        stat_to_inc = input("What stat do you want to increase? (Choose 1: Str, Dex, Con, Int, Wis, Cha) ")
+        counter = [1]
+        for x in counter:
+            if stat_to_inc.lower().strip() == "str":
+                if stat_types["Str"] == 20:
+                    continue
+                else:
+                    stat_types["Str"] += 2
+            elif stat_to_inc.lower().strip() == "dex":
+                if stat_types["Dex"] == 20:
+                    continue
+                else:
+                    stat_types["Dex"] += 2
+            elif stat_to_inc.lower().strip() == "con":
+                if stat_types["Con"] == 20:
+                    continue
+                else:
+                    stat_types["Con"] += 2
+            elif stat_to_inc.lower().strip() == "int":
+                if stat_types["Int"] == 20:
+                    continue
+                else:
+                    stat_types["Int"] += 2
+            elif stat_to_inc.lower().strip() == "wis":
+                if stat_types["Wis"] == 20:
+                    continue
+                else:
+                    stat_types["Wis"] += 2
+            elif stat_to_inc.lower().strip() == "cha":
+                if stat_types["Cha"] == 20:
+                    continue
+                else:
+                    stat_types["Cha"] += 2
+    elif inc_amount == 2:
+        stats_to_inc = input("What stats do you want to increase? (Choose 2, separate by a comma: Str, Dex, Con, Int, Wis, Cha) ")
+        inc_list = stats_to_inc.split(",")
+        if len(inc_list) > 2:
+            incs = inc_list
+            inc_list = [incs[0], incs[1]]
+        for stat in inc_list:
+            if stat.lower().strip() == "str":
+                if stat_types["Str"] == 20:
+                    continue
+                else:
+                    stat_types["Str"] += 1
+            elif stat.lower().strip() == "dex":
+                if stat_types["Dex"] == 20:
+                    continue
+                else:
+                    stat_types["Dex"] += 1
+            elif stat.lower().strip() == "con":
+                if stat_types["Con"] == 20:
+                    continue
+                else:
+                    stat_types["Con"] += 1
+            elif stat.lower().strip() == "int":
+                if stat_types["Int"] == 20:
+                    continue
+                else:
+                    stat_types["Int"] += 1
+            elif stat.lower().strip() == "wis":
+                if stat_types["Wis"] == 20:
+                    continue
+                else:
+                    stat_types["Wis"] += 1
+            elif stat.lower().strip() == "cha":
+                if stat_types["Cha"] == 20:
+                    continue
+                else:
+                    stat_types["Cha"] += 1
+
+    print(stat_types)
+    return stat_types
+
+def set_level(u_level = None):
+    if u_level == None:
+        u_level = int(input("What level is your character? (number) "))
+    user_level = u_level
+    if u_level >= 19:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif u_level >= 16:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif u_level >= 12:
+        stat_increase()
+        stat_increase()
+        stat_increase()
+    elif u_level >= 8:
+        stat_increase()
+        stat_increase()
+    elif u_level >= 4:
+        stat_increase()
+    return user_level
+
+def set_race(race = None):
+    global user_race
+    global movement_speed
+    global darkvision
+    global languages
+    if race == None:
+        user_race = input("What race is your character? (Dwarf, Half-orc, Elf, Halfling, Human, Dragonborn, Gnome, Half-elf, Tiefling, Aasimar, Changeling, Kenku, Warforged, Arachne, Shifter, Aarakocra, Kobold, Fire Genasi, Air Genasi, Earth Genasi, Water Genasi) ")
+    else:
+        user_race = race    
+    if user_race.lower().strip() == "dwarf":
+        stat_types["Con"] += 2
+        movement_speed = 25
+        darkvision = True
+        languages = ["Common", "Dwarvish"]
+    elif user_race.lower().strip() == "half-orc":
+        stat_types["Str"] += 2
+        stat_types["Con"] += 1
+        movement_speed = 30
+        darkvision = True
+        languages = ["Common", "Orc"]
+    elif user_race.lower().strip() == "elf":
+        stat_types["Dex"] += 2
+        movement_speed = 30
+        darkvision = True
+        languages = ["Common", "Elven"]
+    elif user_race.lower().strip() == "halfling":
+        stat_types["Dex"] += 2
+        movement_speed = 25
+        darkvision = False
+        languages = ["Common", "Halfling"]
+    elif user_race.lower().strip() == "human":
+        stat_types["Str"] += 1
+        stat_types["Dex"] += 1
+        stat_types["Con"] += 1
+        stat_types["Int"] += 1
+        stat_types["Wis"] += 1
+        stat_types["Cha"] += 1
+        movement_speed = 30
+        darkvision = False
+        languages = ["Common", input("Choose 1 language other than Common: ")]
+    elif user_race.lower().strip() == "dragonborn":
+        stat_types["Str"] += 2
+        stat_types["Cha"] += 1
+        movement_speed = 30
+        darkvision = False
+        languages = ["Common", "Draconic"]
+    elif user_race.lower().strip() == "gnome":
+        stat_types["Int"] += 2
+        movement_speed = 25
+        darkvision = True
+        languages = ["Common", "Gnomish"]
+    elif user_race.lower().strip() == "half-elf":
+        stat_types["Cha"] += 2
+        stat_increase(2)
+        movement_speed = 30
+        darkvision = True
+        languages = ["Common", "Elven", input("Choose 1 language other than Common and Elven: ")]
+    elif user_race.lower().strip() == "tiefling":
+        stat_types["Cha"] += 2
+        movement_speed = 30
+        darkvision = True
+        languages = ["Common", "Infernal"]
+    elif user_race.lower().strip() == "aasimar" or user_race.lower().strip() == "changeling" or user_race.lower().strip() == "kenku" or user_race.lower().strip() == "shifter" or user_race.lower().strip() == "aarakocra" or user_race.lower().strip() == "kobold" or user_race.lower().strip() == "fire genasi" or user_race.lower().strip() == "air genasi" or user_race.lower().strip() == "water genasi" or user_race.lower().strip() == "earth genasi":
+        stat_types[input("Choose 1 score to increase by 1 (Str, Dex, Con, Int, Wis, Cha): ")] += 1
+        inc_2 = int(input("Would you like to increase one score by 2 points (1), or 2 scores by 1 point each (2)? "))
+        if inc_2 == 1:
+            stat_increase(1)
+        elif inc_2 == 2:
+            stat_increase(2)
+        if user_race.lower().strip() == "air genasi":
+            movement_speed = 35
+        else:
+            movement_speed = 30
+        darkvision = True
+        languages = ["Common", input("What language other than Common have you agreed on with your DM? ")]
+    elif user_race.lower().strip() == "warforged":
+        stat_types["Con"] += 2
+        stat_types[input("Choose 1 score to increase by 1 (Str, Dex, Con, Int, Wis, Cha): ")] += 1
+        movement_speed = 30
+        darkvision = False
+        languages = ["Common", input("Choose 1 language other than Common: ")]
+    elif user_race.lower().strip() == "arachne":
+        stat_types["Dex"] += 2
+        stat_types["Wis"] += 1
+        movement_speed = 30
+        darkvision = True
+        languages = ["Common", "Undercommon"]
+    elif user_race.lower().strip() == "starling" or user_race.lower().strip() == "bloodling" or user_race.lower().strip() == "core starling" or user_race.lower().strip() == "shadeling" or user_race.lower().strip() == "crystalling" or user_race.lower().strip() == "symbioling" or user_race.lower().strip() == "pure starling":
+        current_race = user_race
+        if user_race.lower().strip() != "pure starling":
+            prev_race = input("What race were you before? ")
+            set_race(prev_race)
+            movement_speed += 20
+        else:
+            set_race("human")
+            movement_speed += 25
+        user_race = current_race
+        stat_increase(2)
+        darkvision = True
+        languages.append("Sangulect")
+    else:
+        movement_speed = 30
+        darkvision = False
+        languages = ["Common"]
+    return user_race, movement_speed, darkvision, languages, stat_types
