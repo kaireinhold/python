e8606f790b0b09e4ad15e1288c84d8362bed1a8c
Kai Reinhold <keena.reinhold07@gmail.com>
2025-02-21 08:59:09 -0700

    added second option for rolling

--- DnD_Game/diceroll_infinite.py
+++ DnD_Game/diceroll_infinite.py
@@ -9,8 +9,12 @@ dice = Dnd()
 while True:
     roll_a_dice = input("Would you like to roll a dice? (y/n) ").lower().strip()
     if roll_a_dice == "y" or roll_a_dice == "yes":
+        roll_type = input("Would you like to see roll outputs as they go? (y/n) ").lower().strip()
         dice.rolls = []
-        dice.roll()  # Call the roll method of the dice object
+        if roll_type == "y":
+            dice.roll()  # Call the roll method of the dice object
+        else:
+            dice.roll_no_output()
         print(dice.rolls)  # Print the rolls from the dice instance
     else:
         print("Goodbye!")
